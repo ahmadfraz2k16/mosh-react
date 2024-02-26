69136a93e112485b92567e5356f07b54085805c2
unknown <ahmadfraz2k16@gmail.com>
2024-02-27 01:12:28 +0500

    first commit, removed index.css, removed code from app.css, installed bootstrap and linked in index.html instead of deleted index.css

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCase
+function Message(){
+    //Jsx: javascript xml
+    const name = "Ahmad"
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello world</h1>
+}
+
+export default Message;
