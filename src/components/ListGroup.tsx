63fb97e8161d5ed91716046d017d8abc4e608c2f
unknown <ahmadfraz2k16@gmail.com>
2024-02-27 04:36:06 +0500

    Section Building components, Video: Conditional Rendering, explained in OneNote

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,12 +2,13 @@ function ListGroup() {
     let items = [
         "Lahore", "Islamabad", "KPK", "Mardan", "Murree"
     ]
-    items=[];
+    items=[]; 
+    
   return (
     <>
         <h1>List</h1> 
         <ul className="list-group">
-            {items.length == 0 ? <p>No item found</p> : null}
+          {items.length == 0 && <p>No item found</p> }
           {items.map((item)=>(
             <li className="list-group-item" key={item}>{item}</li>
           ))}
